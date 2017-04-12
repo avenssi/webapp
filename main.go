b3d033d4e5885241072d84b94b61244a237c873a
ychen11 <chenyiwei1987@gmail.com>
2017-04-12 20:34:44 +0800

    lesson one

--- main.go
+++ main.go
@@ -5,9 +5,11 @@ import (
     "net/http"
 )
 
+
 func firstPage(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "<h1>Hello, I am avenssi! </h1>")
 }
+
 
 func main() {
 	http.HandleFunc("/", firstPage)
