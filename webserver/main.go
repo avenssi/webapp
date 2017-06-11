53e14923ba0cfcfc5d0bf821c78fb60a192b6678
ychen11 <chenyiwei1987@gmail.com>
2017-06-11 22:15:33 +0800

    change the response

--- webserver/main.go
+++ webserver/main.go
@@ -3,15 +3,17 @@ package main
 import (
     "io"
     "net/http"
+    "log"
 )
 
 
 func firstPage(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "<h1>Hello, I am a coder! </h1>")
+	log.Println("New request came in.")
+	io.WriteString(w, "<h1>Hello, I am a hero! </h1>")
 }
 
 
 func main() {
 	http.HandleFunc("/", firstPage)
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(":8000", nil) //:::8000
 }
